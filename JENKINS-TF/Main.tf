c8c586c92ee6012eedd842481bf868f765b76702
Sylvester Bassey <115729068+Slybss@users.noreply.github.com>
2024-06-16 16:56:52 +0100

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-04b70fa74e45c3917"
   instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  key_name               = "ud"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
 
